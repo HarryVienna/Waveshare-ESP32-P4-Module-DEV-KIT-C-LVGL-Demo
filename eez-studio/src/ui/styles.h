1f0e48b2ca654396e2204fcfd72e1ea4d230dd58
Harald Kreuzer <mail@haraldkreuzer.net>
2026-02-22 21:16:21 +0100

    Remove style

--- eez-studio/src/ui/styles.h
+++ eez-studio/src/ui/styles.h
@@ -7,11 +7,6 @@
 extern "C" {
 #endif
 
-// Style: Weatherstation Button
-lv_style_t *get_style_weatherstation_button_MAIN_DEFAULT();
-void add_style_weatherstation_button(lv_obj_t *obj);
-void remove_style_weatherstation_button(lv_obj_t *obj);
-
 #ifdef __cplusplus
 }
 #endif
